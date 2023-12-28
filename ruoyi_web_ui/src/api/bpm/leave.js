03b4fe9e8348eec36fb585a5e8ee03b8e3189750
Jp <1716768076@qq.com>
2023-12-28 17:41:00 +0800

    请假表单，和已办表单

--- ruoyi_web_ui/src/api/bpm/leave.js
+++ ruoyi_web_ui/src/api/bpm/leave.js
@@ -1,27 +1,44 @@
 import request from '@/utils/request'
 
-// 创建请假申请
-export function createLeave(data) {
-  return request({
-    url: '/bpm/oa/leave/create',
-    method: 'post',
-    data: data
-  })
+// 查询OA 请假申请列表
+export function listLeave(query) {
+return request({
+url: '/bpm/oa/leave/list',
+method: 'get',
+params: query
+})
 }
 
-// 获得请假申请
+// 查询OA 请假申请详细
 export function getLeave(id) {
-  return request({
-    url: '/bpm/oa/leave/get?id=' + id,
-    method: 'get'
-  })
+return request({
+url: '/bpm/oa/leave/' + id,
+method: 'get'
+})
 }
 
-// 获得请假申请分页
-export function getLeavePage(query) {
-  return request({
-    url: '/bpm/oa/leave/page',
-    method: 'get',
-    params: query
-  })
+// 新增OA 请假申请
+export function addLeave(data) {
+return request({
+url: '/bpm/oa/leave',
+method: 'post',
+data: data
+})
+}
+
+// 修改OA 请假申请
+export function updateLeave(data) {
+return request({
+url: '/bpm/oa/leave',
+method: 'put',
+data: data
+})
+}
+
+// 删除OA 请假申请
+export function delLeave(id) {
+return request({
+url: '/bpm/oa/leave/' + id,
+method: 'delete'
+})
 }
