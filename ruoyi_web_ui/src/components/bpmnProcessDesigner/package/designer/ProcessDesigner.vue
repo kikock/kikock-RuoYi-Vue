be87bf163970b05f6e83ae97546cd96ccc83e6bb
kikock <kikock@qq.com>
2023-12-29 17:07:24 +0800

    流程设计 增加返回按钮

--- ruoyi_web_ui/src/components/bpmnProcessDesigner/package/designer/ProcessDesigner.vue
+++ ruoyi_web_ui/src/components/bpmnProcessDesigner/package/designer/ProcessDesigner.vue
@@ -113,6 +113,12 @@
             :type="props.headerButtonType"
             :disabled="simulationStatus"
         />
+        <XButton
+            pre-icon="back2"
+            title="返回"
+            @click="backModel"
+            :type="props.headerButtonType"
+        />
       </template>
       <!-- 用于打开本地文件-->
       <input
@@ -601,7 +607,7 @@ const previewProcessJson = () => {
     previewModelVisible.value = true
   })
 }
-/* ------------------------------------------------ 芋道源码 methods ------------------------------------------------------ */
+/* ------------------------------------------------  methods ------------------------------------------------------ */
 const processSave = async () => {
   console.log(bpmnModeler, 'bpmnModelerbpmnModelerbpmnModelerbpmnModeler')
   const { err, xml } = await bpmnModeler.saveXML()
@@ -616,6 +622,12 @@ const processSave = async () => {
   // 触发 save 事件
   emit('save', xml)
 }
+//返回
+const backModel = () => {
+  emit('backModel')
+}
+
+
 /** 高亮显示 */
 // const highlightedCode = (previewType, previewResult) => {
 //   console.log(previewType, 'previewType, previewResult')
