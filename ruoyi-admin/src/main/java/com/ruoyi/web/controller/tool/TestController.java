7bcb5c57464ec2a4668fc02097aa819bed547398
kikock <34462958@qq.com>
2024-07-15 13:42:29 +0800

    sql语句 更新

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/TestController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/TestController.java
@@ -7,7 +7,6 @@ import com.ruoyi.common.core.domain.R;
 import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.framework.web.service.SysLoginService;
 import io.swagger.annotations.*;
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
