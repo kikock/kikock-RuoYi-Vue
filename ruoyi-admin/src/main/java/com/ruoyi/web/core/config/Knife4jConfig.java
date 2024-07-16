fdd2b6723bdc16c0fb5d87576b1fea9f7f08365c
kikock <34462958@qq.com>
2024-07-16 13:41:39 +0800

    清理废弃文件

--- ruoyi-admin/src/main/java/com/ruoyi/web/core/config/Knife4jConfig.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/core/config/Knife4jConfig.java
@@ -4,22 +4,9 @@ import com.ruoyi.common.config.RuoYiConfig;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-import lombok.Data;
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springdoc.core.properties.SpringDocConfigProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Swagger2的接口配置
@@ -45,7 +32,7 @@ public class Knife4jConfig{
                         .description("项目简介，支持Markdown格式：`这里是代码标签哦`，**这里是强调哦**")
                         //版本
                         .version("版本号:" + ruoyiConfig.getVersion())
-                        .contact(new Contact().name("曹奎"))
+                        .contact(new Contact().name("kikock"))
                 );
     }
 }
